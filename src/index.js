79c21ea0b9713c8438ff6e1a1d5737e003cf085e
katy millard <katy.millard@gmail.com>
2022-02-24 13:41:44 -0600

    checkerboard shuffle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 function main() {
   const myBoxes = document.getElementsByClassName('mybox');
   const myButton = document.getElementById('shuffle');
+  let i = 0;
 
   myButton.addEventListener('click', () => {
+    for (const myBox of myBoxes) {
+      if ((i % 2) === 0) {
+        myBox.style.backgroundColor = 'red';
+      }
+      else {
+        myBox.style.backgroundColor = 'black';
+      }
+      i += 1;
+    }
   });
 }
