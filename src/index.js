446b26f183795eaa9b2f00d5fb3cab42d06ba4bc
Dylan Hackworth <dylhack@gmail.com>
2022-02-24 11:08:19 -0600

    multiple-boxes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function main() {
-  // Get the "my-box" div from the document
-  // When someone clicks on the element then change the background color
-  // to red.
-}
+  const myBoxes = document.getElementsByClassName('mybox');
+  const myButton = document.getElementById('shuffle');
 
+  myButton.addEventListener('click', () => {
+  });
+}
